5a41890f6099d01f5eecfd6221b6d1a7cf00344c
Lukasz <my.email>
2021-03-22 13:02:02 +0100

    @Test part II

--- src/test/java/com/example/demo/controller/HobbitControllerTest.java
+++ src/test/java/com/example/demo/controller/HobbitControllerTest.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,12 +25,18 @@ class HobbitControllerTest {
     // 3 sposoby, na dokumentację testu - komentarza, nazwa metody, @DisplayName
     @Test
     @DisplayName("GET /hobbits -> HTTP 200")
-    void givenFullSpringContext_whenGETHobbits_shouldReturn200() {
+    void givenFullSpringContext_whenGETHobbits_shouldReturn200() throws Exception {
         // given full context with db (@SpringBootTest)
 
         // when GET http://localhost:8080/hobbits
-
+                                                    // wywołanie endpoint'u (bez Tomcat'a)
+        mockMvc.perform(get("/hobbits"))
+                                                    // lepsze logowanie
+                .andDo(print())
         // then should return HTTP 200
-        fail();
+                                                    // czego oczekujemy od zwróconego komunikatu
+                .andExpect(status().isOk());
+
+
     }
 }
