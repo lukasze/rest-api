60b43843404793b26e2d0f38d6241037b294aafe
Lukasz <my.email>
2021-03-22 14:13:16 +0100

    @Test part III

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO hobbit (id, name, last_name) VALUES (1, 'Frodo', 'Nowak');
+INSERT INTO hobbit (id, name, last_name) VALUES (2, 'Bilbo', 'Baggins');
--- src/test/java/com/example/demo/controller/HobbitControllerTest.java
+++ src/test/java/com/example/demo/controller/HobbitControllerTest.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.demo.model.Hobbit;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,22 +28,29 @@ class HobbitControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    // 3 sposoby, na dokumentację testu - komentarza, nazwa metody, @DisplayName
+//    @DisplayName("GET /hobbits -> HTTP 200, all hobbits from DB")
     @Test
-    @DisplayName("GET /hobbits -> HTTP 200")
-    void givenFullSpringContext_whenGETHobbits_shouldReturn200() throws Exception {
-        // given full context with db (@SpringBootTest)
-
-        // when GET http://localhost:8080/hobbits
-                                                    // wywołanie endpoint'u (bez Tomcat'a)
-        mockMvc.perform(get("/hobbits"))
-                                                    // lepsze logowanie
+    void givenFullSpringContextAnd2HobbitsInDB_whenGETHobbits_shouldReturn200And2Hobbits() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/hobbits"))
                 .andDo(print())
-        // then should return HTTP 200
-                                                    // czego oczekujemy od zwróconego komunikatu
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+//       1 pobranie danych z odpowiedzi
+                .andReturn();
+        String hobbitsInJSONFormat = mvcResult.getResponse().getContentAsString();
 
+//       2 zmapowanie JSON'a do postaci obiektowej
+        List<Hobbit> allHobbitsinDb = objectMapper.readValue(hobbitsInJSONFormat, new TypeReference<>() {});
 
+//       3 asercje sprawdzające zwrócone dane zmapowane do postaci obiektowej
+        assertAll(
+                () -> assertEquals(2, allHobbitsinDb.size()),
+                () -> assertEquals("Frodo", allHobbitsinDb.get(0).getName()),
+                () -> assertEquals("Nowak", allHobbitsinDb.get(0).getLastName()),
+                () -> assertEquals("Bilbo", allHobbitsinDb.get(1).getName()),
+                () -> assertEquals("Baggins", allHobbitsinDb.get(1).getLastName())
+        );
     }
 }
